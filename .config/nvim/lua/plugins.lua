ab8a02147fb57e18fd345f85ea473100a2848da8
DennisFeldbusch <58029815+DennisFeldbusch@users.noreply.github.com>
2023-02-04 09:49:55 +0100

    [NVIM] adds treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -30,8 +30,15 @@ return require('packer').startup(function(use)
 
 
 
-  -- Post-install/update hook with neovim command
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
+  }
+  use {'nvim-treesitter/nvim-treesitter-textobjects'}
+
 
   --
   use {
